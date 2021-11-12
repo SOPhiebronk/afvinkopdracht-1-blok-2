c63dd48c6b851e8cdaabd53f6392957a3036d073
SOPhiebronk <90451185+SOPhiebronk@users.noreply.github.com>
2021-11-12 13:47:04 +0100

    Add files via upload

---
+++ afvinkopdracht 1.py
@@ -0,0 +1,52 @@
+
+def lees_inhoud():
+    with open('hamming_distance_training.txt') as f:
+      bestand_lezen = f.read()
+
+
+      lijst1 =[]
+      bestand_lezen.readlines
+      lijst1.
+      #lijst.append(bestand_lezen)
+
+      #for l in bestand_#hij leest zo de tweede lijst en kan ik zo twee lijsten maken
+
+      #return lijst
+    
+    
+    
+
+
+
+
+    #with open('hamming_distance_training.txt') as f:
+      #bestand_lezen = f.read()
+
+      #for lines in bestand_lezen:
+          #return(lines)
+
+def vergelijken(lijst):
+    seq1 = lijst[0]
+    seq2 = lijst[1]
+    print(seq1)
+    print
+
+    
+    
+
+def main():
+    training_bestand = "hamming_distance_training.txt"
+    print(lees_inhoud())
+    lijst = lees_inhoud()
+    vergelijken(lijst)
+    print(vergelijken(lijst))
+    
+    #test_bestand = "hamming_distance_test.fa"
+
+    # Lees de twee sequenties in
+
+    # Bereken de hamming distance van deze twee sequenties door ze te
+    # vergelijken per positie en de verschillen bij elkaar op te tellen
+
+
+main()
